109801a650d838c3c3dee22904d4a3e1cd7990b5
Krasi Georgiev <48542862+krasigeorgiev@users.noreply.github.com>
2020-04-06 15:58:15 +0300

    Add files via upload

---
+++ 11-githubMigrations101/PostsController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use DB;
+use App\Post;
+
+class PostsController extends Controller
+{
+    public function show($slug)
+    {
+    //    $post = DB::table('posts')->where('slug', $slug)->first(); //QueryBuilder - an alternative
+        return view('post', [
+            'post' => Post::where('slug', $slug)->firstOrFail()
+        ]);
+    }
+}
