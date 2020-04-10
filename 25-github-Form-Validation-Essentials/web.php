f60c54e8d1cefcd9c7fcc5f845316829e9d75452
Krasi Georgiev <48542862+krasigeorgiev@users.noreply.github.com>
2020-04-10 16:22:57 +0300

    Add files via upload

---
+++ 25-github-Form-Validation-Essentials/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('/', function() {
+    return view('welcome');
+});
+
+Route::get('/articles', function() {
+    return view('articles.index', [
+        'articles' => App\Article::latest()->get()
+    ]);
+});
+
+Route::get('/articles', 'ArticlesController@index');
+Route::post('/articles', 'ArticlesController@store');
+Route::get('/articles/create', 'ArticlesController@create');
+Route::get('/articles/{article}', 'ArticlesController@show');
+Route::get('/articles/{article}/edit', 'ArticlesController@edit');
+Route::put('/articles/{article}', 'ArticlesController@update');
+
+
