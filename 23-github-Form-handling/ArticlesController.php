721557c4b7c9a275723ef75ba419104376ae4f1a
Krasi Georgiev <48542862+krasigeorgiev@users.noreply.github.com>
2020-04-10 12:04:25 +0300

    Add files via upload

---
+++ 23-github-Form-handling/ArticlesController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Article;
+use Illuminate\Http\Request;
+
+class ArticlesController extends Controller
+{
+    public function index()
+    {
+        // Render a list of a resource.
+        $articles = Article::latest()->get();
+
+        return view('articles.index', ['articles' => $articles]);
+    }
+    public function show($id)
+    {
+        // Show a single resource.
+        $article = Article::find($id);
+
+        return view('articles.show', ['article' => $article]);
+    }
+
+    public function create()
+    {
+        // Shows a view to create a new resource
+        return view('articles.create');
+    }
+
+    public function store()
+    {
+        // Persist the new resource
+        $article = new Article();
+
+        $article->title = \request('title');
+        $article->excerpt = \request('excerpt');
+        $article->body = \request('body');
+
+        $article->save();
+
+        return redirect('/articles');
+    }
+
+    public function edit()
+    {
+        // Show a view to edit an existing resource
+    }
+    public function update()
+    {
+        // Persist the edited resource
+    }
+    public function destroy()
+    {
+        // Delete the resource
+    }
+}
